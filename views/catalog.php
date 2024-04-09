c097a59d6856450ff98a4893bced496f32a6623b
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2024-04-09 18:39:43 +0300

    Update catalog.php

--- views/catalog.php
+++ views/catalog.php
@@ -3,31 +3,37 @@
 
     <div class="width-wrapper">
         <?php
-        include("./connect_db.php");
+        try {
+            include("./connect_db.php");
 
-        $sql = "SELECT * FROM books";
-        $result = mysqli_query($connection, $sql);
+            $sql = "SELECT * FROM books";
+            $result = mysqli_query($connection, $sql);
 
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                $imageData = $row["image"];
-                $id = $row["id"];
-                $title = $row["title"];
-                $author = $row["author"];
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $imageData = $row["image"];
+                    $id = $row["id"];
+                    $title = $row["title"];
+                    $author = $row["author"];
 
-                echo "
-                    <div class='card'>
-                        <a href='./details'>" .
-                        '<img src="data:image/jpeg;base64,' . base64_encode($imageData) . '" alt="Uploaded Image">'.
-                        "<h3>{$title}</h3> 
-                        <h4>{$author}</h4>
-                    </a>
-                </div>
-                    ";
+                    echo "
+                        <div class='card'>
+                            <a href='./details'>" .
+                            '<img src="data:image/jpeg;base64,' . base64_encode($imageData) . '" alt="Uploaded Image">'.
+                            "<h3>{$title}</h3> 
+                            <h4>{$author}</h4>
+                        </a>
+                    </div>
+                        ";
+                }
+            } else {
+                echo "<h3>No results were found</h3>";
             }
-        } else {
-            echo "No results we found";
+        
+        } catch(mysqli_sql_exception) {
+            echo "Oops something went wrong during selection from db";
         }
+        
         ?>
     </div>
 </section>
