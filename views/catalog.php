2177e2c67344cf860c0d17f8618b520209880f33
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2024-04-09 10:16:58 +0300

    updated catalog page styling

--- views/catalog.php
+++ views/catalog.php
@@ -1,3 +1,14 @@
-<section>
+<section id="catalog">
     <h2>This is Catalog Page</h2>
+
+    <div class="width-wrapper">
+        <div class="card">
+            <a href="">
+                <img src="./images/kratki-otgovori-na-golemite-vaprosi-30.jpg" alt="Some Image here">
+
+                <h3>Кратки отговори на големите въпроси</h3> 
+                <h4>Стивън Хокинг</h4>
+            </a>
+        </div>
+    </div>
 </section>
